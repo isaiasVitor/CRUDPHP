91f3e5278fdbaee54a682d807876fb132cd839d0
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-21 10:59:59 -0300

    ok

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
                     <div class="col-md-4 mb-3">
                         <label for="telefoneFixo">Telefone Fixo</label>
                         <input type="tel" class="form-control telefoneFixo" name="telefone_fixo" id="telefoneFixo"
-                            placeholder="(xx) xxxx-xxxx"> <script src="<?php echo BASEURL; ?>js/jquery.mask.min.js" >$("#telefone").mask("(00) 0000-0000");</script>
+                            placeholder="(xx) xxxx-xxxx"> <script src="<?php echo BASEURL; ?>js/jquery.mask.min.js" >jQuery("#telefone").mask("(00) 0000-0000");</script>
                         <div class="invalid-feedback">
                             Por favor digite um telefone valido.
                         </div>
