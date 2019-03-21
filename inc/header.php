06f150d26f70b99c2622717db6c19260db37e9f9
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-21 15:18:25 -0300

    ok

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html style="height:85%">
+<html style="height:100%">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
-<body style="height:85%" >
+<body style="height:100%" >
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#">ADBA</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
