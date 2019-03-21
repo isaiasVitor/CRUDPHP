86adcd8f2a51c8413721a656e2cf28d18c6733bb
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-21 14:28:32 -0300

    ok

--- customers/index.php
+++ customers/index.php
@@ -19,41 +19,37 @@
     <?php echo $_SESSION['message']; ?>
 </div>
 <?php clear_messages(); ?> <?php endif; ?>
-<div class="table-responsive">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th width="30%">Nome</th>
-                <th>Telefone Fixo</th>
-                <th>Telefone Celular</th>
-                <th>Congregação</th>
-                <th>Opções</th>
-            </tr>
-        </thead>
-        <tbody> <?php if ($customers) : ?> <?php foreach ($customers as $customer) : ?>
-            <tr>
-                <td><?php echo $customer['id']; ?></td>
-                <td><?php echo $customer['nome']; ?></td>
-                <td><?php echo $customer['telefone_fixo']; ?></td>
-                <td><?php echo $customer['telefone_celular']; ?></td>
-                <td><?php echo $customer['congregacao'];?></td>
-                <td class="actions text-right">
-                    <a href="edit.php?id=<?php echo $customer['id']; ?>" class="btn btn-sm btn-warning">
-                        <i class="fa fa-pencil"></i> Editar
-                    </a>
-                    <a href="#" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#delete-modal"
-                        data-customer="<?php echo $customer['id']; ?>">
-                        <i class="fa fa-trash"></i> Excluir
-                    </a>
-                </td>
-            </tr> <?php endforeach; ?>
-            <?php else : ?>
-            <tr>
-                <td colspan="6">Nenhum registro encontrado.</td>
-            </tr> <?php endif; ?>
-        </tbody>
 
-    </table>
+<div class="list-group" <?php if ($customers) : ?> <?php foreach ($customers as $customer) : ?>>
+    <a href="#" class="list-group-item list-group-item-action active">
+        <?php echo $customer['id']; ?> <?php echo $customer['nome'];?>
+    </a>
+    <div class="collapse" id="collapseExample">
+        <div class="card card-body">
+            <?php echo $customer['telefone_fixo']; "<BR>" ?>
+            <?php echo $customer['telefone_celular']; "<BR>" ?>
+            <?php echo $customer['congregacao']; "<BR>"?>
+        </div>
+    </div>
+
+</div>
+
+
+
+<a href="edit.php?id=<?php echo $customer['id']; ?>" class="btn btn-sm btn-warning">
+    <i class="fa fa-pencil"></i> Editar
+</a>
+<a href="#" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#delete-modal"
+    data-customer="<?php echo $customer['id']; ?>">
+    <i class="fa fa-trash"></i> Excluir
+</a>
+
+<?php endforeach; ?>
+<?php else : ?>
+Nenhum registro encontrado
+<?php endif; ?>
+</tbody>
+
+</table>
 </div>
 <?php include(FOOTER_TEMPLATE); ?>
