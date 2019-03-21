1fb447fff350dc9c02b2531fd65d7afa0b78894c
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-21 12:43:43 -0300

    ok

--- customers/index.php
+++ customers/index.php
@@ -4,17 +4,6 @@
 ?>
 
 <?php include(HEADER_TEMPLATE); ?>
-<header>
-    <div class="row">
-        <div class="col-sm-6">
-            <h2>Clientes</h2>
-        </div>
-        <div class="col-sm-6 text-right h2">
-            <a class="btn btn-primary" href="add.php"><i class="fa fa-plus"></i> Novo Cliente</a>
-            <a class="btn btn-default" href="index.php"><i class="fa fa-refresh"></i> Atualizar</a>
-        </div>
-    </div>
-</header>
 <?php if (!empty($_SESSION['message'])) : ?>
 <div class="alert alert-<?php echo $_SESSION['type']; ?> alert-dismissible" role="alert">
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -31,7 +20,6 @@
             <th width="30%">Nome</th>
             <th>CPF/CNPJ</th>
             <th>Telefone</th>
-            <th>Atualizado em</th>
             <th>Opções</th>
         </tr>
     </thead>
