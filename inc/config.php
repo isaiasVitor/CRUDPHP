53eef921979d4587d0d4712be2f52b1445bf4c97
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-21 09:51:50 -0300

    real

---
+++ inc/config.php
@@ -0,0 +1,11 @@
+<?php		/** O nome do banco de dados*/	
+define('DB_NAME', 'adbadb');		/** Usuário do banco de dados MySQL */	
+define('DB_USER', 'isaias');		/** Senha do banco de dados MySQL */	
+define('DB_PASSWORD', 'kyzasd39XZ#@');		/** nome do host do MySQL */	
+define('DB_HOST', 'localhost');		/** caminho absoluto para a pasta do sistema **/	
+if ( !defined('ABSPATH') )		
+define('ABSPATH', dirname(__FILE__) . '/');			/** caminho no server para o sistema **/	
+if ( !defined('BASEURL') )		
+define('BASEURL', '/CRUD');			/** caminho do arquivo de banco de dados **/	
+if ( !defined('DBAPI') )		
+define('DBAPI', ABSPATH . 'inc/database.php');
