c78d794e9186a66d1b8a7ce37ca5e86edf96739f
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-21 10:16:20 -0300

    ok

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('DB_HOST', 'localhost');		/** caminho absoluto para a pasta do sistema **
 if ( !defined('ABSPATH') )		
 define('ABSPATH', dirname(__FILE__) . '/');			/** caminho no server para o sistema **/	
 if ( !defined('BASEURL') )		
-define('BASEURL', '/CRUD');			/** caminho do arquivo de banco de dados **/	
+define('BASEURL', '/CRUD/');			/** caminho do arquivo de banco de dados **/	
 if ( !defined('DBAPI') )		
     define('DBAPI', ABSPATH . 'inc/database.php');
 
--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 </main> <!-- /container -->
 <hr>
 <footer class="container">
-    <p>&copy;2016 - Web Dev Academy</p>
+    <p>&copy;2019 - Isaias Developer</p>
 </footer>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>
