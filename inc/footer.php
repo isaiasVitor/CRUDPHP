274362cf722817edc932dbccab067edf37457813
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-21 14:51:47 -0300

    ok

--- inc/footer.php
+++ inc/footer.php
@@ -1,12 +1,9 @@
 </main> <!-- /container -->
-<hr>
+
 <footer class="container">
     <p>&copy;2019 - Isaias Developer</p>
 </footer>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script>
-    window.jQuery || document.write('<script src="<?php echo BASEURL; ?>js/jquery-1.11.2.min.js"><\/script>')
-</script>
 <script src="<?php echo BASEURL; ?>js/bootstrap.min.js"></script>
 <script src="<?php echo BASEURL; ?>js/main.js"></script>
 
