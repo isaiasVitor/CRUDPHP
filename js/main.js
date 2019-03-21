a60460fad7321217945c204e5ac8760214a2ac11
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-21 11:36:37 -0300

    ok

--- js/main.js
+++ js/main.js
@@ -25,4 +25,5 @@ $.getScript("js/jquery.mask.js", function(){
 $(document).ready(function(){
     alert("document started");
     $("input.telefoneFixo").mask("(00) 0000-0000");
+    $("input.telefoneCelular").mask("(00) 00000-0000");
 });
