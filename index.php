b2a68f58fa77d2e58405c7211af2098ea3e7a0eb
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-21 13:56:41 -0300

    ok

--- index.php
+++ index.php
@@ -5,21 +5,21 @@
  ob_start("mb_output_handler");   
  header("Content-Type: text/html; charset=ISO-8859-1",true);
 ?>
-<?php require_once 'config.php'; ?> 
+<?php require_once 'config.php'; ?>
 <?php require_once DBAPI; ?>
-<?php include(HEADER_TEMPLATE); ?> 
+<?php include(HEADER_TEMPLATE); ?>
 <?php $db = open_database(); ?>
 <?php if ($db) : ?>
 
-<div class="card">
+
 <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Bem vindo</h1>
-    <p class="lead">Este webservice foi criado apenas para teste, utilize com atenção!!</p>
-  </div>
+    <div class="container">
+        <h1 class="display-4">Bem vindo</h1>
+        <p class="lead">Este webservice foi criado apenas para teste, utilize com atenção!!</p>
+    </div>
 </div>
 <?php else : ?> <div class="alert alert-danger" role="alert">
     <p><strong>ERRO:</strong> Não foi possível Conectar ao Banco de Dados!</p>
-</div>
-<?php endif; ?>
-<?php include(FOOTER_TEMPLATE); ?>
+
+    <?php endif; ?>
+    <?php include(FOOTER_TEMPLATE); ?>
