464ef077b848a90269f0c732e7a5065eb690ff23
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-21 10:52:02 -0300

    ok

--- inc/header.php
+++ inc/header.php
@@ -8,6 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php echo BASEURL; ?>css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo BASEURL; ?>css/style.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript" src="jQuery-Mask-Plugin/dist/jquery.mask.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
