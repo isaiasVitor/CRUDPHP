1be186266b47af964734c9767119c39ddbc05e57
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-22 12:38:36 -0300

    yes

--- customers/relatorio.php
+++ customers/relatorio.php
@@ -4,7 +4,15 @@
     ?>
 <?php include(HEADER_TEMPLATE); ?>
 
+<div class=row>
 
+<div class="col-md-6" style="background:yellow">
+
+</div>
+<div class="col-md-6" style="backgroud:blue">
+
+</div>
+</div>
 
 
 <?php include(FOOTER_TEMPLATE); ?>
