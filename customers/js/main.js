8625e6bc572c61d54561395c74a446bb26306467
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-22 02:50:16 -0300

    ok

--- customers/js/main.js
+++ customers/js/main.js
@@ -3,19 +3,10 @@ $.getScript("js/jquery.mask.js", function(){
         $("input.telefoneFixo").mask("(00) 0000-0000");
         $("input.telefoneCelular").mask("(00) 00000-0000");
     });
-    $(function() {
-        setTime();
-        function setTime() {
-           var date = new Date().getTime();
-           var string = "Timestamp: "+date;
-           setTimeout(setTime, 3000);
-           $('#num').html(string);
-        }
-      });
-
-        // Função responsável por atualizar as frases
+         // Função responsável por atualizar as frases
         function atualizar()
         {
+            console.log("teste");
             // Fazendo requisição AJAX
             $.post('customers/atualizar.php', function (frase) {
 
