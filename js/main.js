f1f4b1ea6c2d239760825502b1d45bcdad7a3ae7
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-22 09:25:39 -0300

    ok

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $.getScript("js/jquery.mask.js", function(){
      $.post('atualizar.php', function (frase) {
   
          // Exibindo frase
-         $('#frase').html(frase.total);
+         $('#frase').html("Ultimo registro: " + frase.nome + "Apostila: " + frase.id );
   
      }, 'JSON');
  }
