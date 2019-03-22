ee211a499a6e2d114a7344598585e6f1f7c0a9bc
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-22 14:04:41 -0300

    ok

--- customers/relatorio.php
+++ customers/relatorio.php
@@ -5,10 +5,11 @@
 <?php include(HEADER_TEMPLATE); ?>
 
 <div class="card">
-    <div class="container-fluid">
+    
         <div class="card-header">
             <p class="lead">Relatorios do Discipulado</p>
         </div>
+        <div class="container-fluid">
         <div class="card-body">
             <div class=row>
 
