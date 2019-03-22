13feef5fcddb6e5981c865dc92785b46dbab2743
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-22 15:23:19 -0300

    ok

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 </main> <!-- /container -->
 
-<footer class="container footer navbar-fixed-bottom">
+<footer class="container">
     <p>&copy;2019 - Isaias Developer</p>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
