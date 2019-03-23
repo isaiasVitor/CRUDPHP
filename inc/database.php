d42eba6f5926d06279de217cf4780e534584ba95
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-23 09:38:51 -0300

    ok

--- inc/database.php
+++ inc/database.php
@@ -135,11 +135,13 @@ function relatorioTotalPorCongregacao(){
   $sql = "select count(id) as total, congregacao from alunos group by congregacao;";
 
   try{
-    $results = $database->query($sql);		    		    
-    while($row = $results->fetch_assoc()){
-      $found["congregacao"] = $row["congregacao"];
+    $result = $database->query($sql);		    		    
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          echo "total: " . $row["total"]. " - Congregação: " . $row["congregacao"]. "<br>";
+      }
     }
-   
   }catch(Exception $e){
     
     $_SESSION['message'] = 'Nao foi possivel realizar a operacao.';
