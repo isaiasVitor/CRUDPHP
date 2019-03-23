eb13cfa6e14f8fbe401ac84799731d0f7a3acc45
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-23 08:39:33 -0300

    ok

--- customers/relatorio.php
+++ customers/relatorio.php
@@ -1,29 +1,41 @@
 <?php 	  
     require_once('functions.php'); 	  
     relatorios();
-    var_dump($relatorios["total"]);
+    
     ?>
 <?php include(HEADER_TEMPLATE); ?>
 
 <div class="card">
     <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
         <div class="panel panel-default" role="tab" id="headingTotalPorCongregacao">
-            <div class="panel-heading" >
-                <a class="btn btn-outline-primary btn-block" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTotalPorCongregacao" aria-expanded="false" aria-controls="collapseTotalPorCongregacao">
-                 Total por Congregação
-                </a>            
+            <div class="panel-heading">
+                <a class="btn btn-outline-primary btn-block" role="button" data-toggle="collapse"
+                    data-parent="#accordion" href="#collapseTotalPorCongregacao" aria-expanded="false"
+                    aria-controls="collapseTotalPorCongregacao">
+                    Total por Congregação
+                </a>
             </div>
-            <div id="collapseTotalPorCongregacao" class="panel-collapse <?php echo ("TotalPorCongregacao" == 0 ? 'collapse in' : 'collapse'); ?>" role="tabpanel" aria-labelledby="headingTotalPorCongregacao">
+            <div id="collapseTotalPorCongregacao"
+                class="panel-collapse <?php echo ("TotalPorCongregacao" == 0 ? 'collapse in' : 'collapse'); ?>"
+                role="tabpanel" aria-labelledby="headingTotalPorCongregacao">
                 <div class="panel-body">
+                    <?php  if ($relatorios) : foreach ($relatorios as $relatorio) : ?>
 
-                    <span >Sede </span> 
+
+
+                    <?php endforeach;?>
+                    <?php else : ?>
+                    Nenhum registro encontrado
+                    <?php endif; ?>
+                    <!--<span>Sede </span>
                     <input type="text" class="form-control" value="" readonly>
-                    
-                    <span >Melhado</span>
-                    <input type="text" class="form-control" value="" readonly>              
-                        
+
+                    <span>Melhado</span>
+                    <input type="text" class="form-control" value="" readonly>
+
                     <span>São Paulo</span>
                     <input type="text" class="form-control" value="" readonly>
+                    -->
                 </div>
             </div>
         </div>
