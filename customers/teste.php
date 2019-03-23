0015e203171db71a19d3020d4b765ec9c3f40e0a
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-23 08:48:18 -0300

    ok

---
+++ customers/teste.php
@@ -0,0 +1,25 @@
+<?php
+
+function relatorioTotalPorCongregacao(){
+    $database = open_database();
+    $found = null;		
+  
+    $sql = "select count(id) as total, congregacao from alunos group by congregacao;";
+  
+    try{
+      $result = $database->query($sql);		    		    
+      if ($result->num_rows > 0) {		      
+          $found = $result->fetch_assoc();	
+     }
+    }catch(Exception $e){
+      
+      $_SESSION['message'] = 'Nao foi possivel realizar a operacao.';
+      $_SESSION['type'] = 'danger';
+    }
+    close_database($database);
+    return $found;
+  }
+
+  echo relatorioTotalPorCongregacao();
+
+?>
