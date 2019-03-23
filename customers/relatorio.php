7e2e1bdde87065cf9ee10a44805d590a7bda324f
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-23 08:44:52 -0300

    ok

--- customers/relatorio.php
+++ customers/relatorio.php
@@ -21,7 +21,7 @@
                 <div class="panel-body">
                     <?php  if ($relatorios) : foreach ($relatorios as $relatorio) : ?>
 
-                    <?php $relatorio{"congregacao"}. " : ".$relatorio["total"]; ?>
+                    <?php echo $relatorio{"congregacao"}. " : ".$relatorio["total"]; ?>
 
                     <?php endforeach;?>
                     <?php else : ?>
