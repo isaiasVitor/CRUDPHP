3e999664e50676f812c722753d8b0414cc1d0445
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-23 23:31:25 -0300

    ok

--- inc/database.php
+++ inc/database.php
@@ -1,4 +1,13 @@
-<?php		
+<?php
+ ini_set('default_charset','ISO-8859-1');
+ mb_internal_encoding("UTF-8"); 
+ mb_http_output( "iso-8859-1" );  
+ ob_start("mb_output_handler");   
+ header("Content-Type: text/html; charset=ISO-8859-1",true);
+?>
+
+<?php
+
 mysqli_report(MYSQLI_REPORT_STRICT);		
 function open_database() {		
     try {			
@@ -179,3 +188,5 @@ function congregacoes_all(){
   close_database($database);
   return $congregacoes;
 }
+
+?>
