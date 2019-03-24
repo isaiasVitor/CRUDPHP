bdb73381ecddbb420bc326b09a04545db18502b0
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-23 23:32:49 -0300

    ok

--- inc/database.php
+++ inc/database.php
@@ -1,10 +1,4 @@
-<?php
- ini_set('default_charset','ISO-8859-1');
- mb_internal_encoding("UTF-8"); 
- mb_http_output( "iso-8859-1" );  
- ob_start("mb_output_handler");   
- header("Content-Type: text/html; charset=ISO-8859-1",true);
-?>
+
 
 <?php
 
@@ -188,5 +182,3 @@ function congregacoes_all(){
   close_database($database);
   return $congregacoes;
 }
-
-?>
