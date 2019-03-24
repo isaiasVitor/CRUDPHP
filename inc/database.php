92d27c66cdc6c51bbe1ecc90c07ad61cf23ad3c0
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-23 23:53:27 -0300

    ok

--- inc/database.php
+++ inc/database.php
@@ -2,10 +2,12 @@
 
 <?php
 header( 'content-type: text/html; charset=utf-8' );
-mysqli_report(MYSQLI_REPORT_STRICT);		
+mysqli_report(MYSQLI_REPORT_STRICT);
+		
 function open_database() {		
     try {			
         $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        mysqli_set_charset($conn, "utf8");
         return $conn;		
     } catch (Exception $e) {			
         echo $e->getMessage();			
