099897417faa44586c51533ef60dea2dc39c4312
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-25 16:08:05 -0300

    ok

--- login.php
+++ login.php
@@ -3,27 +3,55 @@
     require_once('../config.php');	
     require_once(DBAPI);
 
+    session_start();
+
+    // Botão enviar
     if(isset($_POST['btn-entrar'])):
         $erros = array();
-        $login = mysqli_espace_string($conn,$_POST['login']);
-        $senha = mysqli_espace_string($conn,$_POST['senha']);
-
-        $sql = "SELECT login FROM usuarios WHERE login = '$login';";
-        $result = msqli_query($conn,$sql);
-        if(mysqli_num_rows($result) > 0):
-            $senha = md5($senha);
-            $sql = "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha';";
-            $result = mysqli_query($conn,$sql);
-
-            if(mysqli_num_rows($result) == 1):
-                $dados = mysqli_fetch_array($resultado);
-            else:
-                $erros[] = "Senha incorreta";
-            endif;
-        else:
-            $erros[] = "Usuario inexistente";
+        $login = mysqli_escape_string($conn, $_POST['login']);
+        $senha = mysqli_escape_string($conn, $_POST['senha']);
+    
+        if(isset($_POST['lembrar-senha'])):
+    
+            setcookie('login', $login, time()+3600);
+            setcookie('senha', md5($senha), time()+3600);
         endif;
+    
+        if(empty($login) or empty($senha)):
+            $erros[] = "<li> O campo login/senha precisa ser preenchido </li>";
+        else:
+            // 105 OR 1=1 
+            // 1; DROP TABLE teste
+    
+            $sql = "SELECT login FROM usuarios WHERE login = '$login'";
+            $resultado = mysqli_query($conn, $sql);		
+    
+            if(mysqli_num_rows($resultado) > 0):
+            $senha = md5($senha);       
+            $sql = "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'";
+    
+    
+    
+            $resultado = mysqli_query($conn, $sql);
+    
+                if(mysqli_num_rows($resultado) == 1):
+                    $dados = mysqli_fetch_array($resultado);
+                    mysqli_close($conn);
+                    $_SESSION['logado'] = true;
+                    $_SESSION['id_usuario'] = $dados['id'];
+                    header('Location: home.php');
+                else:
+                    $erros[] = "<li> Usuário e senha não conferem </li>";
+                endif;
+    
+            else:
+                $erros[] = "<li> Usuário inexistente </li>";
+            endif;
+    
+        endif;
+    
     endif;
+    ?>
 ?>
 
 
