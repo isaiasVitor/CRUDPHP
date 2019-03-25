4c5084e0321b5a3d48f0ffe51b83576ca5a399e9
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-25 17:04:46 -0300

    ok

--- inc/header.php
+++ inc/header.php
@@ -47,10 +47,10 @@ header( 'content-type: text/html; charset=utf-8' );
                 </li>
 
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+            <div class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" hide>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">logout</button>
+            </div>
         </div>
     </nav>
     </nav>
