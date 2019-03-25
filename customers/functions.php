df8ac215ae21874b5e6fe378361adf8c22cf0c1b
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-25 15:27:13 -0300

    ok

--- customers/functions.php
+++ customers/functions.php
@@ -6,7 +6,7 @@ require_once(DBAPI);
 
 session_start();
 if(!isset($_SESSION['logado'])):
-  header("location:javascript:alert(\"Email enviado com Sucesso!\")");
+  header("location:javascript:alert('Email enviado com Sucesso!')");
   header('location:'.BASEURL.'login.php');
 endif;
 
