27016d9282a6eeb05ace376a6976a9a46b0ae722
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-25 16:15:54 -0300

    ok

--- login.php
+++ login.php
@@ -1,4 +1,57 @@
+<?php	
+    header( 'content-type: text/html; charset=utf-8' );
+    require_once('../config.php');	
+    require_once(DBAPI);
 
+    session_start();
+
+    // Botão enviar
+    if(isset($_POST['btn-entrar'])):
+        $erros = array();
+        $login = mysqli_escape_string($conn, $_POST['login']);
+        $senha = mysqli_escape_string($conn, $_POST['senha']);
+    
+        if(isset($_POST['lembrar-senha'])):
+    
+            setcookie('login', $login, time()+3600);
+            setcookie('senha', md5($senha), time()+3600);
+        endif;
+    
+        if(empty($login) or empty($senha)):
+            
+        else:
+            // 105 OR 1=1 
+            // 1; DROP TABLE teste
+    
+            $sql = "SELECT login FROM usuarios WHERE login = '$login'";
+            $resultado = mysqli_query($conn, $sql);		
+    
+            if(mysqli_num_rows($resultado) > 0):
+            $senha = md5($senha);       
+            $sql = "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'";
+    
+    
+    
+            $resultado = mysqli_query($conn, $sql);
+    
+                if(mysqli_num_rows($resultado) == 1):
+                    $dados = mysqli_fetch_array($resultado);
+                    mysqli_close($conn);
+                    $_SESSION['logado'] = true;
+                    $_SESSION['id_usuario'] = $dados['id'];
+                    header('Location: home.php');
+                else:
+                    $erros[] = "<li> Usuário e senha não conferem </li>";
+                endif;
+    
+            else:
+                $erros[] = "<li> Usuário inexistente </li>";
+            endif;
+    
+        endif;
+    
+    endif;
+?>
 
 
 
@@ -42,7 +95,7 @@
         <label for="inputSenha" class="sr-only">Senha</label>
         <input type="password" id="inputSenha" class="form-control" name="senha" placeholder="Senha" required value="<?php echo isset($_COOKIE['senha']) ? $_COOKIE['senha'] : '' ?>">
         <div class="checkbox mb-3">
-
+        Lembrar senha: <input type="checkbox" name="lembrar-senha">
         </div>
         <button class="btn btn-lg btn-primary btn-block" name="btn-entrar" type="submit">Entrar</button>
         <p class="mt-5 mb-3 text-muted">Isaias &copy; 2019</p>
