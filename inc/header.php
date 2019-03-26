b9df4b1331cb239385c3e09b0d75156ba76921d6
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-26 13:38:40 -0300

    ok

--- inc/header.php
+++ inc/header.php
@@ -48,7 +48,7 @@ header( 'content-type: text/html; charset=utf-8' );
 
             </ul>
             <form class="form-inline my-2 my-lg-0" action="<?php echo BASEURL; ?>/customers/logout.php">
-                <span class="mr-sm-2">Ola, <?php echo $_SESSION['nome'] ?> </span>
+                <span class="mr-sm-2" style="color:white">Ola, <?php echo $_SESSION['nome'] ?> </span>
                 <button class="btn btn-outline-success my-2 my-sm-0"  type="submit">Sair</button>
             </form>
         </div>
