52753cbcd27d0671ea8d4a738dd9229a1542b0a5
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-26 15:00:41 -0300

    ok

--- inc/header.php
+++ inc/header.php
@@ -51,12 +51,12 @@ session_start();
 
             </ul>
             <?php if(isset($_SESSION['logado'])): ?>
-            <form class="form-inline my-2 my-lg-0" action="<?php echo BASEURL; ?>/customers/logout.php">
+            <form class="form-inline my-2 my-lg-0" action="<?php echo BASEURL; ?>customers/logout.php">
                 <span class="mr-sm-2 navbar-brand navbar-text" style="color:white">Ola, <?php echo $_SESSION['nome']?></span>
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sair</button>
             </form>
             <?php else: ?>
-            <form class="form-inline my-2 my-lg-0" action="<?php echo BASEURL; ?>/login.php">
+            <form class="form-inline my-2 my-lg-0" action="<?php echo BASEURL; ?>login.php">
                 <span class="mr-sm-2 navbar-brand navbar-text" style="color:white"></span>
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Entrar</button>
             </form>
